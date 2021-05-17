89078d6cf8db31d13e820292ff23d8d045d9d057
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-17 15:26:53 +0530

    add states and methods to filter robots

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: robots,
+      searchField: '',
+    };
+  }
 
-const App = () => {
-  return (
-    <div className='tc' >
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox onSearchChange={this.handleChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchTerm, onSearchChange }) => {
+
   return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='text'
         placeholder='search robots'
+        onChange={onSearchChange}
       />
     </div>
   );
