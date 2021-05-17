331ca424681a7c68642cc639f38fbf663e92211c
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-17 21:23:58 +0530

    add error handling to CardList via ErrorBoundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList/CardList';
 import SearchBox from '../components/SearchBox/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary.js';
 
 class App extends Component {
   constructor(props) {
@@ -31,9 +32,9 @@ class App extends Component {
         .includes(searchField.toLowerCase());
     });
 
-    return (!robots.length) ? 
-    <h1 className='tc f1'>Loading...</h1> : 
-    (
+    return !robots.length ? (
+      <h1 className='tc f1'>Loading...</h1>
+    ) : (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
         <SearchBox
@@ -41,7 +42,9 @@ class App extends Component {
           onSearchChange={this.handleSearchChange}
         />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
